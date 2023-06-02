e084c636dc8c9c2f0ba363aea391661af5c4c74c
Vrishin Patel <vrishin_patel@outlook.com>
2023-06-02 10:29:08 +0530

    build(deps): Add Master CSS

---
+++ web/src/hooks.server.ts
@@ -0,0 +1,9 @@
+import { renderIntoHTML } from '@master/css';
+import config from './master.css';
+
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+	return await resolve(event, {
+		transformPageChunk: ({ html }) => renderIntoHTML(html, config),
+	});
+}
---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { LazyCSSProvider } from '@master/css.svelte';
+
+	const loadConfig = import('../master.css');
+</script>
+
+<LazyCSSProvider config={loadConfig}>
+	<slot />
+</LazyCSSProvider>
